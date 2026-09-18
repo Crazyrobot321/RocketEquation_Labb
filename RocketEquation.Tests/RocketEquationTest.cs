a041ffc027e68987b31b3a927a92b685121cbdb4
Crazyrobot321 <erikmz40@gmail.com>
2026-09-18 12:43:18 +0200

    FuelForMass_ReturnsExpectedFuel

--- RocketEquation.Tests/RocketEquationTest.cs
+++ RocketEquation.Tests/RocketEquationTest.cs
@@ -7,12 +7,12 @@
         [InlineData(14, 2)]
         [InlineData(1969, 654)]
         [InlineData(100756, 33583)]
-        public void CalculateFuelRequirement_ShouldReturnCorrectFuel(int mass, int expectedFuel)
+        public void FuelForMass_ReturnsExpectedFuel(int mass, int expectedFuel)
         {
             //Arrange
             var sut = new FuelCalculator();
             // Act
-            int actualFuel = sut.SumFuelForAllModules(mass);
+            int actualFuel = sut.FuelForMass(mass);
             // Assert
             Assert.Equal(expectedFuel, actualFuel);
         }
--- RocketEquation/FuelCalculations.cs
+++ RocketEquation/FuelCalculations.cs
@@ -1,8 +1,19 @@
 ﻿
 public class FuelCalculator
 {
-    public int SumFuelForAllModules(int mass)
+    public int SumFuelForAllModules(int[] masses)
     {
-        return (mass / 3) - 2;
+        return masses.Sum(FuelForMass);
+    }
+    public int FuelForMass(int mass)
+    {
+        return mass / 3 - 2;
+    }
+    public int TotalFuelForMass(int mass)
+    {
+        int fuel = FuelForMass(mass);
+        if (fuel <= 0) 
+            return 0;
+        return fuel + TotalFuelForMass(fuel);
     }
 }
